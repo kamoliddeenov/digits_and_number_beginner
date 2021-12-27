b626c935186698a614053dceebbb63d8768aae4c
Muhammad Ali <kamoliddeenov@gmail.com>
2021-12-27 17:56:48 +0500

    get length

--- get_length.py
+++ get_length.py
@@ -1,19 +1,16 @@
 #find count of digits in an integer without using for, len, while
 def get_length(num):
-    x1 = pow(0, num)
+    x = 0
+    x -= pow(0, num)
     num//=10
-
-    x2 = pow(0, num)
+    x -= pow(0, num)
     num//=10
-
-    x3 = pow(0, num)
+    x -= pow(0, num)
     num//=10
-
-    x4 = pow(0, num)
+    x -= pow(0, num)
     num//=10
-
-    x5 = pow(0, num)
+    x -= pow(0, num)
     
-    return 5 - (x1 + x2 + x3 + x4 + x5)
+    return x+5
 
-print(get_length(15))
+print(get_length(100))
